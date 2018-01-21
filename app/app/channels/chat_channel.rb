222740e699e71711ff328b23fc9b4cecdb8d1ada
Alexey Vasilyev <bestspajic@gmail.com>
2018-01-21 16:28:22 +0300

    Finish EvilFront follow-up

---
+++ app/app/channels/chat_channel.rb
@@ -0,0 +1,23 @@
+class ChatChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "chat"
+  end
+
+  # Called when message-form contents are received by the server
+  def send_message(payload)
+    message = Message.new(author: current_user, text: payload["message"])
+    if message.save
+      ActionCable.server.broadcast "chat", message: render(message)
+    end
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  private
+
+  def render(message)
+    ApplicationController.new.helpers.c("message", message: message)
+  end
+end
